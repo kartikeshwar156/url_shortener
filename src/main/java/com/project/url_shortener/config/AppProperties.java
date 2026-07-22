b83e7c78835b256279f09e060c51ad6c693394a3
APOLLO <mgmstorenoida@gmail.com>
2026-07-22 06:19:37 +0530

    Creating Config

---
+++ src/main/java/com/project/url_shortener/config/AppProperties.java
@@ -0,0 +1,7 @@
+package com.project.url_shortener.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "app")
+public record AppProperties(String baseUrl) {
+}
